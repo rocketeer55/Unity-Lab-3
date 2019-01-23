324b796772a6e54fef1bb01e79c2408ba60c8287
rocketeer55 <spencer.schurk@gmail.com>
2019-01-23 08:29:14 -0800

    Lab3

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float upForce;
+
+    private bool isDead;
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+
+    void Start() {
+        upForce = 200f;
+        isDead = false;
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (isDead == false) {
+            if (Input.GetMouseButtonDown(0)) {
+                rigidbody2D.velocity = Vector2.zero;
+                rigidbody2D.AddForce(new Vector2(0, upForce));
+                animator.SetTrigger("Flap");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D() {
+        rigidbody2D.velocity = Vector2.zero;
+        isDead = true;
+        animator.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+    }
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2D;
+
+    void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
+    }
+
+    void Update() {
+        if (GameControl.instance.gameOver == true) {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
